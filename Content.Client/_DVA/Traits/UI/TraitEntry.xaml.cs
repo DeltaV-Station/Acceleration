JustTNE/Delta-v

real traits system (#5208)
* holy shit?

* final touches

* this is dumb

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* whatever go my integration tests

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* whoopsie forgot blacklists

* load bearing loc

* minor stuff

* whoopsie

* cool

* I LOVE REFLECTION

* got a call from the stink department

* i love fluent yes

* direction changes

* waiter more migrations please

* typo ops

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Tobias Berger <toby@tobot.dev>

Content.Client/_DV/Traits/UI/TraitEntry.xaml.cs
@@ -1,0 +1,218 @@
+using Content.Shared._DV.Traits;
+using Content.Shared._DV.Traits.Conditions;
+using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.CustomControls;
+using Robust.Client.UserInterface.XAML;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Client._DV.Traits.UI;
+
+[GenerateTypedNameReferences]
+public sealed partial class TraitEntry : PanelContainer
+{
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly ILocalizationManager _loc = default!;
+
+    public event Action<bool>? OnToggled;
+
+    public bool IsSelected => TraitCheckbox.Pressed;
+    public int TraitCost { get; }
+
+    private readonly TraitPrototype _trait;
+    private bool _isUpdating;
+    private readonly List<string> _failedConditionTooltips = new();
+
+    public TraitEntry(TraitPrototype trait)
+    {
+        RobustXamlLoader.Load(this);
+        IoCManager.InjectDependencies(this);
+
+        _trait = trait;
+        TraitCost = trait.Cost;
+
+        // Enable mouse events so tooltips work
+        MouseFilter = MouseFilterMode.Pass;
+
+        TraitNameLabel.Text = Loc.GetString(trait.Name);
+        TraitDescriptionLabel.SetMessage(Loc.GetString(trait.Description));
+
+        // Format cost display
+        var costPrefix = trait.Cost > 0 ? "+" : "";
+        var costColor = trait.Cost > 0 ? "#ff6b6b" : trait.Cost < 0 ? "#6bff6b" : "#888888";
+        TraitCostLabel.Text = $"{costPrefix}{trait.Cost}";
+        TraitCostLabel.ModulateSelfOverride = Color.FromHex(costColor);
+
+        TraitCheckbox.OnToggled += OnCheckboxToggled;
+
+        // Build condition tooltips
+        UpdateConditionTooltips();
+    }
+
+    private void UpdateConditionTooltips()
+    {
+        var tooltips = new List<string>();
+
+        foreach (var condition in _trait.Conditions)
+        {
+            var tooltip = condition.GetTooltip(_prototype, _loc);
+            if (!string.IsNullOrEmpty(tooltip))
+                tooltips.Add(tooltip);
+        }
+
+        if (tooltips.Count > 0)
+        {
+            var tooltipText = Loc.GetString("trait-conditions-tooltip",
+                ("requirements", string.Join("\n", tooltips)));
+
+            TooltipSupplier = _ => CreateMarkupTooltip(tooltipText);
+        }
+        else
+            TooltipSupplier = null;
+    }
+
+    /// <summary>
+    /// Creates a tooltip control that properly parses markup.
+    /// </summary>
+    private static Tooltip CreateMarkupTooltip(string markupText)
+    {
+        var tooltip = new Tooltip();
+
+        // Parse the markup into a FormattedMessage
+        tooltip.SetMessage(FormattedMessage.FromMarkupOrThrow(markupText));
+
+        return tooltip;
+    }
+
+    /// <summary>
+    /// Updates whether conditions are met based on current job/species.
+    /// </summary>
+    public void UpdateConditionsMet(string? jobId, string? speciesId)
+    {
+        _failedConditionTooltips.Clear();
+        MeetsConditions = true;
+
+        foreach (var condition in _trait.Conditions)
+        {
+            var result = condition switch
+            {
+                IsSpeciesCondition speciesCond => CheckSpeciesCondition(speciesCond, speciesId),
+                HasJobCondition jobCond => CheckJobCondition(jobCond, jobId),
+                InDepartmentCondition deptCond => CheckDepartmentCondition(deptCond, jobId),
+                _ => true,
+            };
+
+            // Apply inversion
+            result ^= condition.Invert;
+
+            if (!result)
+            {
+                MeetsConditions = false;
+                var tooltip = condition.GetTooltip(_prototype, _loc);
+                if (!string.IsNullOrEmpty(tooltip))
+                    _failedConditionTooltips.Add(tooltip);
+            }
+        }
+
+        UpdateDisabledState();
+    }
+
+    private bool CheckSpeciesCondition(IsSpeciesCondition condition, string? speciesId)
+    {
+        if (string.IsNullOrEmpty(speciesId))
+            return false;
+
+        return speciesId == condition.Species.Id;
+    }
+
+    private bool CheckJobCondition(HasJobCondition condition, string? jobId)
+    {
+        if (string.IsNullOrEmpty(jobId))
+            return false;
+
+        return jobId == condition.Job;
+    }
+
+    private bool CheckDepartmentCondition(InDepartmentCondition condition, string? jobId)
+    {
+        if (string.IsNullOrEmpty(jobId))
+            return false;
+
+        if (!_prototype.TryIndex(condition.Department, out var department))
+            return false;
+
+        return department.Roles.Contains(jobId);
+    }
+
+    private void UpdateDisabledState()
+    {
+        TraitCheckbox.Disabled = !MeetsConditions;
+
+        if (!MeetsConditions)
+        {
+            // Deselect if conditions no longer met
+            if (TraitCheckbox.Pressed)
+            {
+                _isUpdating = true;
+                TraitCheckbox.Pressed = false;
+                UpdateSelectedStyle();
+                _isUpdating = false;
+                OnToggled?.Invoke(false);
+            }
+
+            // Show why it's disabled
+            AddStyleClass("TraitsEntryDisabled");
+
+            // Update tooltip to show failed conditions
+            if (_failedConditionTooltips.Count > 0)
+            {
+                var tooltipText = Loc.GetString("trait-conditions-not-met-tooltip",
+                    ("requirements", string.Join("\n", _failedConditionTooltips)));
+
+                TooltipSupplier = _ => CreateMarkupTooltip(tooltipText);
+            }
+        }
+        else
+        {
+            RemoveStyleClass("TraitsEntryDisabled");
+            UpdateConditionTooltips(); // Reset to normal tooltips
+        }
+    }
+
+    private void OnCheckboxToggled(BaseButton.ButtonToggledEventArgs args)
+    {
+        if (_isUpdating)
+            return;
+
+        if (!MeetsConditions)
+        {
+            // Prevent selection if conditions not met
+            _isUpdating = true;
+            TraitCheckbox.Pressed = false;
+            _isUpdating = false;
+            return;
+        }
+
+        UpdateSelectedStyle();
+        OnToggled?.Invoke(args.Pressed);
+    }
+
+    public void SetSelected(bool selected)
+    {
+        _isUpdating = true;
+        TraitCheckbox.Pressed = selected && MeetsConditions;
+        UpdateSelectedStyle();
+        _isUpdating = false;
+    }
+
+    public bool MeetsConditions { get; private set; } = true;
+
+    private void UpdateSelectedStyle()
+    {
+        if (TraitCheckbox.Pressed)
+            AddStyleClass("TraitsEntrySelected");
+        else
+            RemoveStyleClass("TraitsEntrySelected");
+    }
+}

Content.Shared/_DV/Traits/Conditions/HasJobCondition.cs
@@ -1,0 +1,50 @@
+using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DV.Traits.Conditions;
+
+/// <summary>
+/// Condition that checks if the player has a specific job.
+/// Use Invert = true to check if the player does NOT have the job.
+/// </summary>
+public sealed partial class HasJobCondition : BaseTraitCondition
+{
+    /// <summary>
+    /// The job prototype ID to check for.
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<JobPrototype> Job = string.Empty;
+
+    protected override bool EvaluateImplementation(TraitConditionContext ctx)
+    {
+        if (string.IsNullOrEmpty(ctx.JobId))
+            return false;
+
+        return ctx.JobId == Job;
+    }
+
+    public override string GetTooltip(IPrototypeManager proto, ILocalizationManager loc)
+    {
+        var jobName = Job.Id;
+        var jobColor = "#ffffff";
+
+        if (proto.TryIndex(Job, out var jobProto))
+        {
+            jobName = loc.GetString(jobProto.Name);
+
+            // Try to find the job's department color
+            foreach (var dept in proto.EnumeratePrototypes<DepartmentPrototype>())
+            {
+                if (dept.Roles.Contains(Job))
+                {
+                    jobColor = dept.Color.ToHex();
+                    break;
+                }
+            }
+        }
+
+        return Invert
+            ? loc.GetString("trait-condition-job-not", ("job", jobName), ("color", jobColor))
+            : loc.GetString("trait-condition-job-is", ("job", jobName), ("color", jobColor));
+    }
+}

Content.Shared/_DV/Traits/Conditions/IsSpeciesCondition.cs
@@ -1,0 +1,38 @@
+using Content.Shared.Humanoid.Prototypes;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DV.Traits.Conditions;
+
+/// <summary>
+/// Condition that checks if the player is a specific species.
+/// Use Invert = true to check if the player is NOT the species.
+/// </summary>
+public sealed partial class IsSpeciesCondition : BaseTraitCondition
+{
+    /// <summary>
+    /// The species ID to check for.
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<SpeciesPrototype> Species = string.Empty;
+
+    protected override bool EvaluateImplementation(TraitConditionContext ctx)
+    {
+        if (string.IsNullOrEmpty(ctx.SpeciesId))
+            return false;
+
+        return ctx.SpeciesId == Species;
+    }
+
+    public override string GetTooltip(IPrototypeManager proto, ILocalizationManager loc)
+    {
+        var speciesName = Species.Id;
+        if (proto.TryIndex(Species, out var speciesProto))
+        {
+            speciesName = loc.GetString(speciesProto.Name);
+        }
+
+        return Invert
+            ? loc.GetString("trait-condition-species-not", ("species", speciesName))
+            : loc.GetString("trait-condition-species-is", ("species", speciesName));
+    }
+}

Content.Shared/_DV/Traits/Effects/BaseTraitEffect.cs
@@ -1,0 +1,31 @@
+using JetBrains.Annotations;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DV.Traits.Effects;
+
+/// <summary>
+/// Base class for trait effects. Implementations apply modifications to an entity when a trait is selected.
+/// </summary>
+[ImplicitDataDefinitionForInheritors, UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
+public abstract partial class BaseTraitEffect
+{
+    /// <summary>
+    /// Applies the effect to the target entity.
+    /// </summary>
+    [PublicAPI]
+    public abstract void Apply(TraitEffectContext ctx);
+}
+
+/// <summary>
+/// Context passed to trait effects for application.
+/// Contains references to the player entity and relevant systems.
+/// </summary>
+public sealed class TraitEffectContext
+{
+    public required EntityUid Player { get; init; }
+    public required IEntityManager EntMan { get; init; }
+    public required IPrototypeManager Proto { get; init; }
+    public required IComponentFactory CompFactory { get; init; }
+    public required ILogManager LogMan { get; init; }
+    public required TransformComponent Transform { get; init; }
+}

Content.Shared/_DV/Traits/Effects/OverrideCompsEffect.cs
@@ -1,0 +1,22 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DV.Traits.Effects;
+
+/// <summary>
+/// Effect that overrides component fields on the player entity.
+/// If the component exists, its fields are overwritten with the new values.
+/// If it doesn't exist, the component is added.
+/// </summary>
+public sealed partial class OverrideCompsEffect : BaseTraitEffect
+{
+    /// <summary>
+    /// The components to add/override on the entity.
+    /// </summary>
+    [DataField(required: true)]
+    public ComponentRegistry Components = new();
+
+    public override void Apply(TraitEffectContext ctx)
+    {
+        ctx.EntMan.AddComponents(ctx.Player, Components, removeExisting: true);
+    }
+}

Content.Shared/_DV/Traits/Effects/RemCompsEffect.cs
@@ -1,0 +1,31 @@
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Generic;
+
+namespace Content.Shared._DV.Traits.Effects;
+
+/// <summary>
+/// Effect that removes components from the player entity if they exist.
+/// </summary>
+public sealed partial class RemCompsEffect : BaseTraitEffect
+{
+    /// <summary>
+    /// The component names to remove from the entity.
+    /// </summary>
+    [DataField(required: true, customTypeSerializer: typeof(CustomHashSetSerializer<string, ComponentNameSerializer>))]
+    public HashSet<string> Components = new();
+
+    public override void Apply(TraitEffectContext ctx)
+    {
+        foreach (var compName in Components)
+        {
+            if (!ctx.CompFactory.TryGetRegistration(compName, out var registration))
+            {
+                var sawmill = ctx.LogMan.GetSawmill("traits");
+                sawmill.Warning($"RemCompsEffect references unknown component: {compName}");
+                continue;
+            }
+
+            ctx.EntMan.RemoveComponent(ctx.Player, registration.Type);
+        }
+    }
+}

DeltaV-Station/Acceleration

Content.Client/_DVA/Traits/UI/TraitEntry.xaml.cs
@@ -1,0 +1,218 @@
+using Content.Shared._DVA.Traits;
+using Content.Shared._DVA.Traits.Conditions;
+using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.CustomControls;
+using Robust.Client.UserInterface.XAML;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Client._DVA.Traits.UI;
+
+[GenerateTypedNameReferences]
+public sealed partial class TraitEntry : PanelContainer
+{
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly ILocalizationManager _loc = default!;
+
+    public event Action<bool>? OnToggled;
+
+    public bool IsSelected => TraitCheckbox.Pressed;
+    public int TraitCost { get; }
+
+    private readonly DVTraitPrototype _trait;
+    private bool _isUpdating;
+    private readonly List<string> _failedConditionTooltips = new();
+
+    public TraitEntry(DVTraitPrototype trait)
+    {
+        RobustXamlLoader.Load(this);
+        IoCManager.InjectDependencies(this);
+
+        _trait = trait;
+        TraitCost = trait.Cost;
+
+        // Enable mouse events so tooltips work
+        MouseFilter = MouseFilterMode.Pass;
+
+        TraitNameLabel.Text = Loc.GetString(trait.Name);
+        TraitDescriptionLabel.SetMessage(Loc.GetString(trait.Description));
+
+        // Format cost display
+        var costPrefix = trait.Cost > 0 ? "+" : "";
+        var costColor = trait.Cost > 0 ? "#ff6b6b" : trait.Cost < 0 ? "#6bff6b" : "#888888";
+        TraitCostLabel.Text = $"{costPrefix}{trait.Cost}";
+        TraitCostLabel.ModulateSelfOverride = Color.FromHex(costColor);
+
+        TraitCheckbox.OnToggled += OnCheckboxToggled;
+
+        // Build condition tooltips
+        UpdateConditionTooltips();
+    }
+
+    private void UpdateConditionTooltips()
+    {
+        var tooltips = new List<string>();
+
+        foreach (var condition in _trait.Conditions)
+        {
+            var tooltip = condition.GetTooltip(_prototype, _loc);
+            if (!string.IsNullOrEmpty(tooltip))
+                tooltips.Add(tooltip);
+        }
+
+        if (tooltips.Count > 0)
+        {
+            var tooltipText = Loc.GetString("trait-conditions-tooltip",
+                ("requirements", string.Join("\n", tooltips)));
+
+            TooltipSupplier = _ => CreateMarkupTooltip(tooltipText);
+        }
+        else
+            TooltipSupplier = null;
+    }
+
+    /// <summary>
+    /// Creates a tooltip control that properly parses markup.
+    /// </summary>
+    private static Tooltip CreateMarkupTooltip(string markupText)
+    {
+        var tooltip = new Tooltip();
+
+        // Parse the markup into a FormattedMessage
+        tooltip.SetMessage(FormattedMessage.FromMarkupOrThrow(markupText));
+
+        return tooltip;
+    }
+
+    /// <summary>
+    /// Updates whether conditions are met based on current job/species.
+    /// </summary>
+    public void UpdateConditionsMet(string? jobId, string? speciesId)
+    {
+        _failedConditionTooltips.Clear();
+        MeetsConditions = true;
+
+        foreach (var condition in _trait.Conditions)
+        {
+            var result = condition switch
+            {
+                IsSpeciesCondition speciesCond => CheckSpeciesCondition(speciesCond, speciesId),
+                HasJobCondition jobCond => CheckJobCondition(jobCond, jobId),
+                InDepartmentCondition deptCond => CheckDepartmentCondition(deptCond, jobId),
+                _ => true,
+            };
+
+            // Apply inversion
+            result ^= condition.Invert;
+
+            if (!result)
+            {
+                MeetsConditions = false;
+                var tooltip = condition.GetTooltip(_prototype, _loc);
+                if (!string.IsNullOrEmpty(tooltip))
+                    _failedConditionTooltips.Add(tooltip);
+            }
+        }
+
+        UpdateDisabledState();
+    }
+
+    private bool CheckSpeciesCondition(IsSpeciesCondition condition, string? speciesId)
+    {
+        if (string.IsNullOrEmpty(speciesId))
+            return false;
+
+        return speciesId == condition.Species.Id;
+    }
+
+    private bool CheckJobCondition(HasJobCondition condition, string? jobId)
+    {
+        if (string.IsNullOrEmpty(jobId))
+            return false;
+
+        return jobId == condition.Job;
+    }
+
+    private bool CheckDepartmentCondition(InDepartmentCondition condition, string? jobId)
+    {
+        if (string.IsNullOrEmpty(jobId))
+            return false;
+
+        if (!_prototype.TryIndex(condition.Department, out var department))
+            return false;
+
+        return department.Roles.Contains(jobId);
+    }
+
+    private void UpdateDisabledState()
+    {
+        TraitCheckbox.Disabled = !MeetsConditions;
+
+        if (!MeetsConditions)
+        {
+            // Deselect if conditions no longer met
+            if (TraitCheckbox.Pressed)
+            {
+                _isUpdating = true;
+                TraitCheckbox.Pressed = false;
+                UpdateSelectedStyle();
+                _isUpdating = false;
+                OnToggled?.Invoke(false);
+            }
+
+            // Show why it's disabled
+            AddStyleClass("TraitsEntryDisabled");
+
+            // Update tooltip to show failed conditions
+            if (_failedConditionTooltips.Count > 0)
+            {
+                var tooltipText = Loc.GetString("trait-conditions-not-met-tooltip",
+                    ("requirements", string.Join("\n", _failedConditionTooltips)));
+
+                TooltipSupplier = _ => CreateMarkupTooltip(tooltipText);
+            }
+        }
+        else
+        {
+            RemoveStyleClass("TraitsEntryDisabled");
+            UpdateConditionTooltips(); // Reset to normal tooltips
+        }
+    }
+
+    private void OnCheckboxToggled(BaseButton.ButtonToggledEventArgs args)
+    {
+        if (_isUpdating)
+            return;
+
+        if (!MeetsConditions)
+        {
+            // Prevent selection if conditions not met
+            _isUpdating = true;
+            TraitCheckbox.Pressed = false;
+            _isUpdating = false;
+            return;
+        }
+
+        UpdateSelectedStyle();
+        OnToggled?.Invoke(args.Pressed);
+    }
+
+    public void SetSelected(bool selected)
+    {
+        _isUpdating = true;
+        TraitCheckbox.Pressed = selected && MeetsConditions;
+        UpdateSelectedStyle();
+        _isUpdating = false;
+    }
+
+    public bool MeetsConditions { get; private set; } = true;
+
+    private void UpdateSelectedStyle()
+    {
+        if (TraitCheckbox.Pressed)
+            AddStyleClass("TraitsEntrySelected");
+        else
+            RemoveStyleClass("TraitsEntrySelected");
+    }
+}

Content.Shared/_DVA/Traits/Conditions/HasJobCondition.cs
@@ -1,0 +1,50 @@
+using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DVA.Traits.Conditions;
+
+/// <summary>
+/// Condition that checks if the player has a specific job.
+/// Use Invert = true to check if the player does NOT have the job.
+/// </summary>
+public sealed partial class HasJobCondition : BaseTraitCondition
+{
+    /// <summary>
+    /// The job prototype ID to check for.
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<JobPrototype> Job = string.Empty;
+
+    protected override bool EvaluateImplementation(TraitConditionContext ctx)
+    {
+        if (string.IsNullOrEmpty(ctx.JobId))
+            return false;
+
+        return ctx.JobId == Job;
+    }
+
+    public override string GetTooltip(IPrototypeManager proto, ILocalizationManager loc)
+    {
+        var jobName = Job.Id;
+        var jobColor = "#ffffff";
+
+        if (proto.TryIndex(Job, out var jobProto))
+        {
+            jobName = loc.GetString(jobProto.Name);
+
+            // Try to find the job's department color
+            foreach (var dept in proto.EnumeratePrototypes<DepartmentPrototype>())
+            {
+                if (dept.Roles.Contains(Job))
+                {
+                    jobColor = dept.Color.ToHex();
+                    break;
+                }
+            }
+        }
+
+        return Invert
+            ? loc.GetString("trait-condition-job-not", ("job", jobName), ("color", jobColor))
+            : loc.GetString("trait-condition-job-is", ("job", jobName), ("color", jobColor));
+    }
+}

Content.Shared/_DVA/Traits/Conditions/IsSpeciesCondition.cs
@@ -1,0 +1,38 @@
+using Content.Shared.Humanoid.Prototypes;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DVA.Traits.Conditions;
+
+/// <summary>
+/// Condition that checks if the player is a specific species.
+/// Use Invert = true to check if the player is NOT the species.
+/// </summary>
+public sealed partial class IsSpeciesCondition : BaseTraitCondition
+{
+    /// <summary>
+    /// The species ID to check for.
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<SpeciesPrototype> Species = string.Empty;
+
+    protected override bool EvaluateImplementation(TraitConditionContext ctx)
+    {
+        if (string.IsNullOrEmpty(ctx.SpeciesId))
+            return false;
+
+        return ctx.SpeciesId == Species;
+    }
+
+    public override string GetTooltip(IPrototypeManager proto, ILocalizationManager loc)
+    {
+        var speciesName = Species.Id;
+        if (proto.TryIndex(Species, out var speciesProto))
+        {
+            speciesName = loc.GetString(speciesProto.Name);
+        }
+
+        return Invert
+            ? loc.GetString("trait-condition-species-not", ("species", speciesName))
+            : loc.GetString("trait-condition-species-is", ("species", speciesName));
+    }
+}

Content.Shared/_DVA/Traits/Effects/BaseTraitEffect.cs
@@ -1,0 +1,31 @@
+using JetBrains.Annotations;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DVA.Traits.Effects;
+
+/// <summary>
+/// Base class for trait effects. Implementations apply modifications to an entity when a trait is selected.
+/// </summary>
+[ImplicitDataDefinitionForInheritors, UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
+public abstract partial class BaseTraitEffect
+{
+    /// <summary>
+    /// Applies the effect to the target entity.
+    /// </summary>
+    [PublicAPI]
+    public abstract void Apply(TraitEffectContext ctx);
+}
+
+/// <summary>
+/// Context passed to trait effects for application.
+/// Contains references to the player entity and relevant systems.
+/// </summary>
+public sealed class TraitEffectContext
+{
+    public required EntityUid Player { get; init; }
+    public required IEntityManager EntMan { get; init; }
+    public required IPrototypeManager Proto { get; init; }
+    public required IComponentFactory CompFactory { get; init; }
+    public required ILogManager LogMan { get; init; }
+    public required TransformComponent Transform { get; init; }
+}

Content.Shared/_DVA/Traits/Effects/OverrideCompsEffect.cs
@@ -1,0 +1,22 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DVA.Traits.Effects;
+
+/// <summary>
+/// Effect that overrides component fields on the player entity.
+/// If the component exists, its fields are overwritten with the new values.
+/// If it doesn't exist, the component is added.
+/// </summary>
+public sealed partial class OverrideCompsEffect : BaseTraitEffect
+{
+    /// <summary>
+    /// The components to add/override on the entity.
+    /// </summary>
+    [DataField(required: true)]
+    public ComponentRegistry Components = new();
+
+    public override void Apply(TraitEffectContext ctx)
+    {
+        ctx.EntMan.AddComponents(ctx.Player, Components, removeExisting: true);
+    }
+}

Content.Shared/_DVA/Traits/Effects/RemCompsEffect.cs
@@ -1,0 +1,31 @@
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Generic;
+
+namespace Content.Shared._DVA.Traits.Effects;
+
+/// <summary>
+/// Effect that removes components from the player entity if they exist.
+/// </summary>
+public sealed partial class RemCompsEffect : BaseTraitEffect
+{
+    /// <summary>
+    /// The component names to remove from the entity.
+    /// </summary>
+    [DataField(required: true, customTypeSerializer: typeof(CustomHashSetSerializer<string, ComponentNameSerializer>))]
+    public HashSet<string> Components = new();
+
+    public override void Apply(TraitEffectContext ctx)
+    {
+        foreach (var compName in Components)
+        {
+            if (!ctx.CompFactory.TryGetRegistration(compName, out var registration))
+            {
+                var sawmill = ctx.LogMan.GetSawmill("traits");
+                sawmill.Warning($"RemCompsEffect references unknown component: {compName}");
+                continue;
+            }
+
+            ctx.EntMan.RemoveComponent(ctx.Player, registration.Type);
+        }
+    }
+}