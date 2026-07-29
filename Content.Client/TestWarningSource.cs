e97585f06eae16afd88f38377b587ea553396630
Janet Blackquill <uhhadd@gmail.com>
2026-07-29 17:17:13 -0400

    test commit to test workflows with

---
+++ Content.Client/TestWarningSource.cs
@@ -0,0 +1,15 @@
+namespace Content.Client;
+
+class Ourgh
+{
+   public static void Ourghn(long i)
+   {
+   }
+
+   public static void Ourgh()
+   {
+      TestL(25l);   // CS0078
+      // try the following line instead
+      // TestL(25L);
+   }
+}
