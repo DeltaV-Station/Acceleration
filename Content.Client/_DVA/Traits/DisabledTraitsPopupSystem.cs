JustTNE/Delta-v

more traits improvements (#5285)
* wowie

* whoopsie

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* WHY DID I DO THAT

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

Content.Client/_DV/Traits/DisabledTraitsPopupSystem.cs
@@ -1,0 +1,51 @@
+using Content.Client._DV.Traits.UI;
+using Content.Shared._DV.CCVars;
+using Content.Shared._DV.Traits;
+using Robust.Shared.Configuration;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client._DV.Traits;
+
+/// <summary>
+/// Client system that shows a popup when traits are disabled due to unmet conditions.
+/// </summary>
+public sealed class DisabledTraitsPopupSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    private DisabledTraitsPopup? _window;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeNetworkEvent<DisabledTraitsEvent>(OnDisabledTraits);
+    }
+
+    private void OnDisabledTraits(DisabledTraitsEvent ev)
+    {
+        // Don't show if user has opted to skip this popup
+        if (_cfg.GetCVar(DCCVars.SkipDisabledTraitsPopup))
+            return;
+
+        // Don't show if no traits were actually disabled
+        if (ev.DisabledTraits.Count == 0)
+            return;
+
+        OpenDisabledTraitsPopup(ev.DisabledTraits);
+    }
+
+    private void OpenDisabledTraitsPopup(Dictionary<ProtoId<TraitPrototype>, List<string>> disabledTraits)
+    {
+        // Close existing window if one is open
+        if (_window != null)
+        {
+            _window.Close();
+            _window = null;
+        }
+
+        _window = new DisabledTraitsPopup(disabledTraits);
+        _window.OpenCentered();
+        _window.OnClose += () => _window = null;
+    }
+}

Content.Client/_DV/Traits/UI/DisabledTraitsPopup.xaml.cs
@@ -1,0 +1,101 @@
+using Content.Client.UserInterface.Controls;
+using Content.Shared._DV.CCVars;
+using Content.Shared._DV.Traits;
+using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+using Robust.Shared.Configuration;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Client._DV.Traits.UI;
+
+[GenerateTypedNameReferences]
+public sealed partial class DisabledTraitsPopup : FancyWindow
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
+
+    private bool _initialSkipState;
+
+    public DisabledTraitsPopup(Dictionary<ProtoId<TraitPrototype>, List<string>> disabledTraits)
+    {
+        IoCManager.InjectDependencies(this);
+        RobustXamlLoader.Load(this);
+
+        InitializeUI(disabledTraits);
+        InitializeEvents();
+    }
+
+    private void InitializeUI(Dictionary<ProtoId<TraitPrototype>, List<string>> disabledTraits)
+    {
+        TitleLabel.Text = Loc.GetString("disabled-traits-popup-label");
+        MessageLabel.SetMessage(FormattedMessage.FromMarkupOrThrow(
+            Loc.GetString("disabled-traits-popup-message")));
+
+        _initialSkipState = _cfg.GetCVar(DCCVars.SkipDisabledTraitsPopup);
+        SkipCheckBox.Pressed = _initialSkipState;
+
+        PopulateDisabledTraits(disabledTraits);
+    }
+
+    private void PopulateDisabledTraits(Dictionary<ProtoId<TraitPrototype>, List<string>> disabledTraits)
+    {
+        DisabledTraitsContainer.RemoveAllChildren();
+
+        foreach (var (traitId, reasons) in disabledTraits)
+        {
+            if (!_prototype.TryIndex(traitId, out var trait))
+                continue;
+
+            var container = new BoxContainer
+            {
+                Orientation = BoxContainer.LayoutOrientation.Vertical,
+                Margin = new Thickness(0, 0, 0, 10)
+            };
+
+            // Trait name
+            var nameLabel = new Label
+            {
+                Text = Loc.GetString(trait.Name),
+                FontColorOverride = Color.FromHex("#ff6b6b"),
+                StyleClasses = { "LabelSubText" }
+            };
+            container.AddChild(nameLabel);
+
+            // Reasons why it was disabled
+            foreach (var reason in reasons)
+            {
+                var reasonLabel = new RichTextLabel
+                {
+                    Margin = new Thickness(10, 2, 0, 0)
+                };
+                reasonLabel.SetMessage(FormattedMessage.FromMarkupOrThrow(
+                    Loc.GetString("disabled-traits-popup-reason", ("reason", reason))));
+                container.AddChild(reasonLabel);
+            }
+
+            DisabledTraitsContainer.AddChild(container);
+        }
+    }
+
+    private void InitializeEvents()
+    {
+        OnClose += SaveSkipState;
+        ButtonClose.OnPressed += OnClosePressed;
+    }
+
+    private void SaveSkipState()
+    {
+        if (SkipCheckBox.Pressed == _initialSkipState)
+            return;
+
+        _cfg.SetCVar(DCCVars.SkipDisabledTraitsPopup, SkipCheckBox.Pressed);
+        _cfg.SaveToFile();
+    }
+
+    private void OnClosePressed(BaseButton.ButtonEventArgs args)
+    {
+        Close();
+    }
+}

Content.IntegrationTests/Tests/_DV/TraitSystemTest.cs
@@ -598,7 +598,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<TraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<TraitPrototype>, List<string>>() });
 
             Assert.Multiple(() =>
             {
@@ -639,7 +639,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<TraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<TraitPrototype>, List<string>>()});
 
             Assert.That(validTraits?.Count, Is.EqualTo(2), "Should respect category maxTraits limit");
 
@@ -673,7 +673,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<TraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<TraitPrototype>, List<string>>() });
 
             Assert.That(validTraits?.Count, Is.EqualTo(2), "Should respect category maxPoints limit");
 
@@ -706,7 +706,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<TraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<TraitPrototype>, List<string>>() });
 
             Assert.That(validTraits?.Contains("TestTraitHasComp"), Is.True, "Trait with met condition should be valid");
 
@@ -739,7 +739,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<TraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<TraitPrototype>, List<string>>() });
 
             Assert.That(validTraits?.Contains("TestTraitHasComp"),
                 Is.False,

Content.Shared/_DV/Traits/Conditions/AnyOfCondition.cs
@@ -1,0 +1,64 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DV.Traits.Conditions;
+
+/// <summary>
+/// Condition that passes if ANY of the child conditions pass.
+/// Use this to create "must meet at least one of these requirements" checks.
+/// </summary>
+public sealed partial class AnyOfCondition : BaseTraitCondition
+{
+    /// <summary>
+    /// List of conditions to check. Passes if any condition evaluates to true.
+    /// </summary>
+    [DataField(required: true)]
+    public List<BaseTraitCondition> Conditions = new();
+
+    protected override bool EvaluateImplementation(TraitConditionContext ctx)
+    {
+        // Inversion doesn't make sense for AnyOfCondition - use inverted child conditions instead
+        if (Invert)
+        {
+            throw new InvalidOperationException(
+                "AnyOfCondition does not support Invert. To require none of the conditions, " +
+                "invert the individual child conditions instead.");
+        }
+
+        // Empty list should fail
+        if (Conditions.Count == 0)
+            return false;
+
+        // Return true if ANY condition passes
+        foreach (var condition in Conditions)
+        {
+            if (condition.Evaluate(ctx))
+                return true;
+        }
+
+        return false;
+    }
+
+    public override string GetTooltip(IPrototypeManager proto, ILocalizationManager loc)
+    {
+        if (Conditions.Count == 0)
+            return string.Empty;
+
+        var requirements = new List<string>();
+
+        foreach (var condition in Conditions)
+        {
+            var tooltip = condition.GetTooltip(proto, loc);
+            if (!string.IsNullOrEmpty(tooltip))
+                requirements.Add(tooltip);
+        }
+
+        if (requirements.Count == 0)
+            return string.Empty;
+
+        // AAAAAAAAAAA
+        var joinedRequirements = string.Join("\n• ", requirements);
+
+        // Handle inversion in tooltip (ANY becomes NONE when inverted)
+        return Loc.GetString("trait-condition-any-of", ("requirements", joinedRequirements));
+    }
+}

Content.Shared/_DV/Traits/DisabledTraitsEvent.cs
@@ -1,0 +1,17 @@
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._DV.Traits;
+
+/// <summary>
+/// Sent from server to client when a player spawns with traits that were disabled due to unmet conditions.
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class DisabledTraitsEvent(Dictionary<ProtoId<TraitPrototype>, List<string>> disabledTraits)
+    : EntityEventArgs
+{
+    /// <summary>
+    /// Dictionary mapping disabled trait IDs to lists of reasons why they were disabled.
+    /// </summary>
+    public Dictionary<ProtoId<TraitPrototype>, List<string>> DisabledTraits = disabledTraits;
+}

DeltaV-Station/Acceleration

Content.Client/_DVA/Traits/DisabledTraitsPopupSystem.cs
@@ -1,0 +1,51 @@
+using Content.Client._DVA.Traits.UI;
+using Content.Shared._DVA.CCVars;
+using Content.Shared._DVA.Traits;
+using Robust.Shared.Configuration;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client._DVA.Traits;
+
+/// <summary>
+/// Client system that shows a popup when traits are disabled due to unmet conditions.
+/// </summary>
+public sealed partial class DisabledTraitsPopupSystem : EntitySystem
+{
+    [Dependency] private IConfigurationManager _cfg = default!;
+
+    private DisabledTraitsPopup? _window;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeNetworkEvent<DisabledTraitsEvent>(OnDisabledTraits);
+    }
+
+    private void OnDisabledTraits(DisabledTraitsEvent ev)
+    {
+        // Don't show if user has opted to skip this popup
+        if (_cfg.GetCVar(DCCVars.SkipDisabledTraitsPopup))
+            return;
+
+        // Don't show if no traits were actually disabled
+        if (ev.DisabledTraits.Count == 0)
+            return;
+
+        OpenDisabledTraitsPopup(ev.DisabledTraits);
+    }
+
+    private void OpenDisabledTraitsPopup(Dictionary<ProtoId<DVTraitPrototype>, List<string>> disabledTraits)
+    {
+        // Close existing window if one is open
+        if (_window != null)
+        {
+            _window.Close();
+            _window = null;
+        }
+
+        _window = new DisabledTraitsPopup(disabledTraits);
+        _window.OpenCentered();
+        _window.OnClose += () => _window = null;
+    }
+}

Content.Client/_DVA/Traits/UI/DisabledTraitsPopup.xaml.cs
@@ -1,0 +1,101 @@
+using Content.Client.UserInterface.Controls;
+using Content.Shared._DVA.CCVars;
+using Content.Shared._DVA.Traits;
+using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+using Robust.Shared.Configuration;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Client._DVA.Traits.UI;
+
+[GenerateTypedNameReferences]
+public sealed partial class DisabledTraitsPopup : FancyWindow
+{
+    [Dependency] private IConfigurationManager _cfg = default!;
+    [Dependency] private IPrototypeManager _prototype = default!;
+
+    private bool _initialSkipState;
+
+    public DisabledTraitsPopup(Dictionary<ProtoId<DVTraitPrototype>, List<string>> disabledTraits)
+    {
+        IoCManager.InjectDependencies(this);
+        RobustXamlLoader.Load(this);
+
+        InitializeUI(disabledTraits);
+        InitializeEvents();
+    }
+
+    private void InitializeUI(Dictionary<ProtoId<DVTraitPrototype>, List<string>> disabledTraits)
+    {
+        TitleLabel.Text = Loc.GetString("disabled-traits-popup-label");
+        MessageLabel.SetMessage(FormattedMessage.FromMarkupOrThrow(
+            Loc.GetString("disabled-traits-popup-message")));
+
+        _initialSkipState = _cfg.GetCVar(DCCVars.SkipDisabledTraitsPopup);
+        SkipCheckBox.Pressed = _initialSkipState;
+
+        PopulateDisabledTraits(disabledTraits);
+    }
+
+    private void PopulateDisabledTraits(Dictionary<ProtoId<DVTraitPrototype>, List<string>> disabledTraits)
+    {
+        DisabledTraitsContainer.RemoveAllChildren();
+
+        foreach (var (traitId, reasons) in disabledTraits)
+        {
+            if (!_prototype.TryIndex(traitId, out var trait))
+                continue;
+
+            var container = new BoxContainer
+            {
+                Orientation = BoxContainer.LayoutOrientation.Vertical,
+                Margin = new Thickness(0, 0, 0, 10)
+            };
+
+            // Trait name
+            var nameLabel = new Label
+            {
+                Text = Loc.GetString(trait.Name),
+                FontColorOverride = Color.FromHex("#ff6b6b"),
+                StyleClasses = { "LabelSubText" }
+            };
+            container.AddChild(nameLabel);
+
+            // Reasons why it was disabled
+            foreach (var reason in reasons)
+            {
+                var reasonLabel = new RichTextLabel
+                {
+                    Margin = new Thickness(10, 2, 0, 0)
+                };
+                reasonLabel.SetMessage(FormattedMessage.FromMarkupOrThrow(
+                    Loc.GetString("disabled-traits-popup-reason", ("reason", reason))));
+                container.AddChild(reasonLabel);
+            }
+
+            DisabledTraitsContainer.AddChild(container);
+        }
+    }
+
+    private void InitializeEvents()
+    {
+        OnClose += SaveSkipState;
+        ButtonClose.OnPressed += OnClosePressed;
+    }
+
+    private void SaveSkipState()
+    {
+        if (SkipCheckBox.Pressed == _initialSkipState)
+            return;
+
+        _cfg.SetCVar(DCCVars.SkipDisabledTraitsPopup, SkipCheckBox.Pressed);
+        _cfg.SaveToFile();
+    }
+
+    private void OnClosePressed(BaseButton.ButtonEventArgs args)
+    {
+        Close();
+    }
+}

Content.IntegrationTests/Tests/_DV/TraitSystemTest.cs
@@ -598,7 +598,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<DVTraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<DVTraitPrototype>, List<string>>() });
 
             Assert.Multiple(() =>
             {
@@ -639,7 +639,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<DVTraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<DVTraitPrototype>, List<string>>()});
 
             Assert.That(validTraits?.Count, Is.EqualTo(2), "Should respect category maxTraits limit");
 
@@ -673,7 +673,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<DVTraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<DVTraitPrototype>, List<string>>() });
 
             Assert.That(validTraits?.Count, Is.EqualTo(2), "Should respect category maxPoints limit");
 
@@ -706,7 +706,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<DVTraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<DVTraitPrototype>, List<string>>() });
 
             Assert.That(validTraits?.Contains("TestTraitHasComp"), Is.True, "Trait with met condition should be valid");
 
@@ -739,7 +739,7 @@ public sealed partial class TraitSystemTest
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             var validTraits = (HashSet<ProtoId<DVTraitPrototype>>)method?.Invoke(traitSys,
-                new object[] { player, selectedTraits, null, null, null });
+                new object[] { player, selectedTraits, null, null, null, new Dictionary<ProtoId<DVTraitPrototype>, List<string>>() });
 
             Assert.That(validTraits?.Contains("TestTraitHasComp"),
                 Is.False,

Content.Shared/_DVA/Traits/Conditions/AnyOfCondition.cs
@@ -1,0 +1,64 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DVA.Traits.Conditions;
+
+/// <summary>
+/// Condition that passes if ANY of the child conditions pass.
+/// Use this to create "must meet at least one of these requirements" checks.
+/// </summary>
+public sealed partial class AnyOfCondition : BaseTraitCondition
+{
+    /// <summary>
+    /// List of conditions to check. Passes if any condition evaluates to true.
+    /// </summary>
+    [DataField(required: true)]
+    public List<BaseTraitCondition> Conditions = new();
+
+    protected override bool EvaluateImplementation(TraitConditionContext ctx)
+    {
+        // Inversion doesn't make sense for AnyOfCondition - use inverted child conditions instead
+        if (Invert)
+        {
+            throw new InvalidOperationException(
+                "AnyOfCondition does not support Invert. To require none of the conditions, " +
+                "invert the individual child conditions instead.");
+        }
+
+        // Empty list should fail
+        if (Conditions.Count == 0)
+            return false;
+
+        // Return true if ANY condition passes
+        foreach (var condition in Conditions)
+        {
+            if (condition.Evaluate(ctx))
+                return true;
+        }
+
+        return false;
+    }
+
+    public override string GetTooltip(IPrototypeManager proto, ILocalizationManager loc)
+    {
+        if (Conditions.Count == 0)
+            return string.Empty;
+
+        var requirements = new List<string>();
+
+        foreach (var condition in Conditions)
+        {
+            var tooltip = condition.GetTooltip(proto, loc);
+            if (!string.IsNullOrEmpty(tooltip))
+                requirements.Add(tooltip);
+        }
+
+        if (requirements.Count == 0)
+            return string.Empty;
+
+        // AAAAAAAAAAA
+        var joinedRequirements = string.Join("\n• ", requirements);
+
+        // Handle inversion in tooltip (ANY becomes NONE when inverted)
+        return Loc.GetString("trait-condition-any-of", ("requirements", joinedRequirements));
+    }
+}

Content.Shared/_DVA/Traits/DisabledTraitsEvent.cs
@@ -1,0 +1,17 @@
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._DVA.Traits;
+
+/// <summary>
+/// Sent from server to client when a player spawns with traits that were disabled due to unmet conditions.
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class DisabledTraitsEvent(Dictionary<ProtoId<DVTraitPrototype>, List<string>> disabledTraits)
+    : EntityEventArgs
+{
+    /// <summary>
+    /// Dictionary mapping disabled trait IDs to lists of reasons why they were disabled.
+    /// </summary>
+    public Dictionary<ProtoId<DVTraitPrototype>, List<string>> DisabledTraits = disabledTraits;
+}