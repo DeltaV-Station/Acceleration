0e686eab9d79568f2d88ad2fa64bca0bf6dcb02e
TNE <38938720+JustTNE@users.noreply.github.com>
2026-07-30 03:35:54 +0200

    Ssd recency indicator (#86)
    * Port ShowSSDIndicatorEvent for use in psionics
    
    DCO signoff: https://github.com/DeltaV-Station/Acceleration/issues/1#issuecomment-5075883369
    
    * feat: Recent SSD indicator, SSD time in admin overlay
    
    https://github.com/DeltaV-Station/Delta-v/pull/5699
    https://github.com/DeltaV-Station/Delta-v/pull/6012
    
    DCO: https://github.com/DeltaV-Station/Acceleration/issues/1#issuecomment-5074893373

--- Content.Client/SSDIndicator/SSDIndicatorSystem.cs
+++ Content.Client/SSDIndicator/SSDIndicatorSystem.cs
@@ -5,6 +5,7 @@ using Content.Shared.NPC;
 using Content.Shared.SSDIndicator;
 using Content.Shared.StatusIcon.Components;
 using Robust.Shared.Configuration;
+using Content.Shared._DVA.Mind; // DeltaV
 
 namespace Content.Client.SSDIndicator;
 
@@ -15,6 +16,7 @@ public sealed partial class SSDIndicatorSystem : EntitySystem
 {
     [Dependency] private IConfigurationManager _cfg = default!;
     [Dependency] private MobStateSystem _mobState = default!;
+    [Dependency] private Shared.SSDIndicator.SSDIndicatorSystem _shared = default!; // DeltaV - SSD Recency, don't want to rename the upstream class
 
     public override void Initialize()
     {
@@ -31,7 +33,25 @@ public sealed partial class SSDIndicatorSystem : EntitySystem
             !HasComp<ActiveNPCComponent>(uid) &&
             HasComp<MindExaminableComponent>(uid))
         {
-            args.StatusIcons.Add(ProtoMan.Index(component.Icon));
+            // args.StatusIcons.Add(ProtoMan.Index(component.Icon)); // DeltaV - commented out. status icon now added below
+            // Begin DeltaV Additions
+            var ev = new ShowSSDIndicatorEvent();
+            RaiseLocalEvent(uid, ref ev);
+            if (ev.Hidden)
+                return;
+
+            // SSD Recency Indicator
+            var stage = _shared.GetStage(new Entity<SSDIndicatorComponent>(uid, component));
+            var icon = stage switch
+            {
+                SsdStage.VeryRecent => component.VeryRecentIcon,
+                SsdStage.Recent => component.RecentIcon,
+                SsdStage.Cryoable => component.Icon,
+                _ => throw new InvalidOperationException($"{ToPrettyString(uid)} has an invalid SSD stage {stage}."),
+            };
+
+            args.StatusIcons.Add(ProtoMan.Index(icon));
+            // End DeltaV Additions
         }
     }
 }
