72a7fb396c0b5b3e3fae67230fe11c15d0de7812
nlaky <299499053+nlaky@users.noreply.github.com>
2026-08-03 12:25:33 -0600

    Add OOC shuttle call vote

--- Content.Shared/_DVA/CCVars/DCCVars.cs
+++ Content.Shared/_DVA/CCVars/DCCVars.cs
@@ -35,6 +35,14 @@ public sealed partial class DCCVars
      * Misc.
      */
 
+    /* OOC shuttle vote */
+
+    /// <summary>
+    /// How long players should have to vote on the round end shuttle being sent
+    /// </summary>
+    public static readonly CVarDef<TimeSpan> EmergencyShuttleVoteTime =
+        CVarDef.Create("shuttle.vote_time", TimeSpan.FromMinutes(1), CVar.SERVER);
+
     /// <summary>
     /// The total time a player has to be SSD to be considered cryoable (stage 3).
     /// Default is 20 minutes. Value should be bigger than <see cref="SsdIndicatorRecentAfterSeconds"/>.
